5408e06d4692f81dc0e4be0df86f02f5ff0dfc00
Maïssa Abdelli <maissaabdelli-13@outlook.fr>
2025-01-13 09:48:19 +0100

    animal_history

--- src/modules/animal_history/entities/animal_history.entity.ts
+++ src/modules/animal_history/entities/animal_history.entity.ts
@@ -1,9 +1,18 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Animal } from '../../animal/entities/animal.entity';
 
+@Entity('animal_history', { schema: 'public' })
 export class AnimalHistory {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column('float')
   weight: number;
+
+  @Column('timestamp', { default: () => 'NOW()' })
+  creation_date: Date;
+
+  @ManyToOne(() => Animal, (Animal) => Animal.id, { onDelete: 'CASCADE', onUpdate: 'CASCADE' })
+  @JoinColumn({ name: 'id_animal' })
+  animal?: Animal;
 }
